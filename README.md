9c8bd5b7436ac6350b013d716edc124558399524
ルチルさん <29337073+Rutile3@users.noreply.github.com>
2023-09-11 14:46:23 +0900

    【初回】初回コミット

---
+++ README.md
@@ -0,0 +1,48 @@
+# console-xyz
+
+JavaScriptでエラーや警告のコンソール出力の方法を調べる過程で、  
+色々なコンソール出力があることを知ったので、  
+使えそうなメソッドをピックアップして使ってみた。
+
+## コンソール出力
+
+### テキスト出力
+
+- console.log()
+- console.error()
+- console.warn()
+- console.info()
+
+### 入れ子構造
+
+- console.group()
+- console.groupCollapsed()
+- console.groupEnd()
+
+### タイマー
+
+- console.time()
+- console.timeEnd()
+
+### スタックトレース
+
+- console.trace()
+
+### その他
+
+テーブル
+
+- console.table()
+
+アサート
+
+- console.assert()
+
+カウンター
+
+- console.count()
+- console.countReset()
+
+## 参考
+
+- [console - Web API | MDN](https://developer.mozilla.org/ja/docs/Web/API/Console)
---
+++ console-xyz.js
@@ -0,0 +1,49 @@
+
+// いつもの
+console.log("log log log");
+
+// エラーと警告と情報
+console.error("error error error");
+console.warn("warn warn warn");
+console.info("info info info");
+
+// 入れ子構造
+console.group();
+console.log("group");
+console.group();
+console.log("group group");
+console.groupCollapsed();
+console.log("group group group");
+console.groupEnd();
+console.log("group group");
+console.groupEnd();
+console.log("group");
+console.groupEnd();
+
+// タイマー
+console.time("timer01");
+for (let i = 0; i < 10000; i++) {}
+console.timeEnd("timer01");
+console.time("timer02");
+for (let i = 0; i < 100000; i++) {}
+console.timeEnd("timer02");
+
+// トレースログ
+console.trace();
+
+// テーブル
+const row01 = { name: "row01", value: "table" };
+const row02 = { name: "row02", value: "table table" };
+const row03 = { name: "row03", value: "table table table" };
+console.table({ row01, row02, row03 });
+
+// その他
+console.assert(true);
+console.assert(false);
+console.count("counter01");
+console.count("counter01");
+console.count("counter01");
+console.count("counter02");
+console.countReset("counter01");
+console.count("counter01");
+console.count("counter02");
